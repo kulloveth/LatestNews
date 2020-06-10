7b2c0f258d43fe3ad2ee1923b18372382822882f
kulloveth <kulloveth@gmail.com>
2020-06-10 22:42:42 +0100

    setup bottom navigation

--- app/src/main/java/com/kulloveth/newsfeed/ui/MainActivity.java
+++ app/src/main/java/com/kulloveth/newsfeed/ui/MainActivity.java
@@ -1,16 +1,28 @@
 package com.kulloveth.newsfeed.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.kulloveth.newsfeed.R;
+import com.kulloveth.newsfeed.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    NavController navController;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navigationView = binding.navView;
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 }
---
+++ app/src/main/java/com/kulloveth/newsfeed/ui/headlines/HeadlineActivity.java
@@ -0,0 +1,25 @@
+package com.kulloveth.newsfeed.ui.headlines;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kulloveth.newsfeed.R;
+
+public class HeadlineActivity extends Fragment {
+
+    public HeadlineActivity() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_favorite,container,false);
+    }
+}
